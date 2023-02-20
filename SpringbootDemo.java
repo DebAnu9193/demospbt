3892bd32c30d624b71fe1b90c03e26187e1aec21
DebAnu9193 <125862633+DebAnu9193@users.noreply.github.com>
2023-02-20 13:28:24 +0530

    Add files via upload

---
+++ SpringbootDemo.java
@@ -0,0 +1,28 @@
+package springhello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+public class SpringbootDemo {
+
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home()
+  {
+	  return "Hello World";
+  }
+  
+    public static void main(String[] args)
+    {
+        
+        SpringApplication.run(SpringbootDemo.class, args);
+    }
+}
+
+
